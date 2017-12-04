0cde6063b078c71b82b64e68e6ac8a3f44cdf62b
Thomas Ouellette <thomas.ouellette@dawsoncollege.qc.ca>
2017-12-04 18:26:18 -0500

    Created Ship Object, added a few preopreties

---
+++ WpfApp4/Ship.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Ship
+{
+    // public int Id { get { return _id; } set { _id = value; } }
+
+    // number of hits ship has left before it is sunk
+    private int healthPoints;
+    // 1 = left, 2 = up, 3 = right, 4 = down
+    private int facing;
+
+    // propreties
+    public int Health { get { return healthPoints; } private set { } }
+    public int Direction { get { return facing; } private set { } }
+    private string shipType;
+
+
+	public Ship(string type, int healthPoints, int direction)
+	{
+        this.healthPoints = healthPoints;
+        this.shipType = type;
+        this.facing = direction;
+
+    }
+
+    public void hit()
+    {
+        if (healthPoints > 0)
+            healthPoints--;
+        else
+        {
+
+        }
+    }
+}
