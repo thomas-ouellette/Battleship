3b598f6ef76812c96ed0ae68486218ac47c4e29d
Roberto Panetta <rob.pane@gmail.com>
2017-12-04 22:55:34 -0500

    .

--- WpfApp4/WindowBattleShip.xaml.cs
+++ WpfApp4/WindowBattleShip.xaml.cs
@@ -24,6 +24,39 @@ namespace WpfApp4
             InitializeComponent();
         }
 
+        public class AIlevel
+        {
+            private int level;
+
+            public AIlevel()
+            {
+                level = 1;
+            }
+            public AIlevel(int lvl)
+            {
+                if (lvl > 3 || lvl < 1)
+                {
+                    throw new InvalidOperationException("Level entered does not meet requirments. Enter a level between 1 and 3");
+                }
+                level = lvl;
+            }
+
+            public void AIAction()
+            {
+                if (level == 1)
+                {
+
+                }
+                else if (level == 2)
+                {
+
+                }
+                else if (level == 3)
+                {
+
+                }
+            }
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
@@ -34,6 +67,24 @@ namespace WpfApp4
             LeaderBoard stats = new LeaderBoard();
             stats.Show();
             
+        }
+
+        private void RadEa_Checked(object sender, RoutedEventArgs e)
+        {
+            AIlevel Easy = new AIlevel(1);
+            Easy.AIAction();
+        }
+
+        private void RadMed_Checked(object sender, RoutedEventArgs e)
+        {
+            AIlevel Medium = new AIlevel(2);
+            Medium.AIAction();
+        }
+
+        private void RadHard_Checked(object sender, RoutedEventArgs e)
+        {
+            AIlevel Hard = new AIlevel(3);
+            Hard.AIAction();
         }
     }
 }
